f5c8d49201bc08720bc1b527045d120218b81666
Ikedinobi <Ikdinobi@gmail.com>
2023-08-16 15:59:12 +0100

    Added all the entities without their mmapping and connected to database

--- src/main/java/com/ikedi/giftcard/entity/Products.java
+++ src/main/java/com/ikedi/giftcard/entity/Products.java
@@ -1,4 +1,45 @@
 package com.ikedi.giftcard.entity;
 
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.annotations.ResultCheckStyle;
+import org.hibernate.annotations.SQLDelete;
+import org.hibernate.annotations.Where;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "products") // name of the table
+@SQLDelete(sql = "UPDATE products SET deleted_at = NOW() where id=?", check = ResultCheckStyle.COUNT)  // soft delete
+@Where(clause = "deleted_at IS NULL")
+@NoArgsConstructor
+@Getter
+@Setter
 public class Products {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank
+    private String name;
+
+    private long discount;
+
+    private long fee;
+
+    @NotBlank
+    private boolean status;
+
+    @Column(nullable = false, updatable = false, name = "created_at")
+    private LocalDateTime createdAt;
+
+    @Column(nullable = false, updatable = false, name = "updated_at")
+    private LocalDateTime updatedAt;
+
+    @Column(nullable = false, updatable = false, name = "deleted_at")
+    private LocalDateTime deletedAt;
 }
