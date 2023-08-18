27e826af708b1d5b58e630fbbdcbfa05966675c2
Ikedinobi <Ikdinobi@gmail.com>
2023-08-18 14:28:10 +0100

    testing from branch 'ikedi'

--- src/main/java/com/ikedi/giftcard/entity/Provider.java
+++ src/main/java/com/ikedi/giftcard/entity/Provider.java
@@ -25,7 +25,7 @@ public class Provider {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @NotBlank(message = "Enter provider name")
+    @NotBlank(message = "Enter provider name ")
     @Column(nullable = false)
     private String name;
 
